7d6b7f4f55d44e91b6c41707057bc6dd4b08412e
José Anderson <jose.andersonjr2@gmail.com>
2022-12-22 16:25:46 -0300

    AdicionarCidade() criado

--- backend/src/main/java/com/dev/backend/controller/CidadeController.java
+++ backend/src/main/java/com/dev/backend/controller/CidadeController.java
@@ -5,6 +5,7 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -22,6 +23,11 @@ public class CidadeController {
     @GetMapping("/")
     public List<Cidade> listarCidades(){
         return cs.listarCidades();
+    }
+
+    @PostMapping("/")
+    public Cidade adicionarCidade(Cidade cidade){
+        return cs.adicionarCidade(cidade);
     }
     
 }
